d37a580d53a8a653a1dff3fad297f387cf847813
rdemz <r.demeillez@live.fr>
2025-04-06 09:12:34 +0200

    Update mod.rs

--- src/neuralchain_core/mod.rs
+++ src/neuralchain_core/mod.rs
@@ -2,14 +2,20 @@
 // Ce fichier définit la structure du module neuralchain_core
 
 // Sous-modules
-pub mod quantum_organism;
+pub mod cortical_hub;
+pub mod autoregulation;
+pub mod neural_organism_bootstrap;
 pub mod emergent_consciousness;
+pub mod quantum_organism;
+pub mod quantum_learning;
 pub mod quantum_entanglement;
-pub mod hyperdimensional_adaptation;
-pub mod temporal_manifold;
-pub mod synthetic_reality;
-pub mod immune_guard;
 pub mod neural_interconnect;
-pub mod quantum_hyperconvergence;
 pub mod unified_integration;
-pub mod windows_vital_integration;
+pub mod temporal_manifold;
+pub mod immune_guard;
+pub mod physical_symbiosis;
+pub mod dream_bootstrap;
+pub mod neural_dream;
+pub mod evolutionary_genesis;
+pub mod hyperdimentional_adaptation;
+pub mod synthetic_reality;
