caf9d67f7434bca1419913fde4a0852c5a0b1970
rdemz <r.demeillez@live.fr>
2025-04-06 08:59:22 +0200

    Create mod.rs

---
+++ src/neuralchain_core/mod.rs
@@ -0,0 +1,15 @@
+// Module neuralchain_core
+// Ce fichier définit la structure du module neuralchain_core
+
+// Sous-modules
+pub mod quantum_organism;
+pub mod emergent_consciousness;
+pub mod quantum_entanglement;
+pub mod hyperdimensional_adaptation;
+pub mod temporal_manifold;
+pub mod synthetic_reality;
+pub mod immune_guard;
+pub mod neural_interconnect;
+pub mod quantum_hyperconvergence;
+pub mod unified_integration;
+pub mod windows_vital_integration;
